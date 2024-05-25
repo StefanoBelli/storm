dc0e0021157123f001fbe296af5ca4add2b896e8
StefanoBelli <stefano9913@gmail.com>
2024-05-26 00:47:27 +0200

    Extending CI: jacoco integration

---
+++ storm-client/test/jvm/ste/ExampleTest.java
@@ -0,0 +1,11 @@
+package ste;
+
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+    @Test
+    public void testCase() {
+        assertEquals(1,1);
+    }
+}
