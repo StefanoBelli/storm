d6d98728bd44834632063556fc52356111ee20fc
StefanoBelli <stefano9913@gmail.com>
2024-05-26 22:18:50 +0200

    ba-dua should look for classes by itself from root of bytecode (package)

--- ste-ci-scripts/genbaduareports.sh
+++ ste-ci-scripts/genbaduareports.sh
@@ -2,9 +2,9 @@
 
 BA_DUA_CLI=testdeps/ba-dua/ba-dua-cli-0.4.0-all.jar
 
-BA_DUA_CLASSES_PATH=$TESTED_SUBPROJECT/target/classes/$BA_DUA_CLASSES
+BA_DUA_CLASSES_PATH=$TESTED_SUBPROJECT/target/classes/
 BA_DUA_SER=$TESTED_SUBPROJECT/coverage.ser
 BA_DUA_OUTXML=ba-dua.xml
 
-echo " * Generating BaDua XML report"
+echo " * Generating ba-dua XML report"
 java -jar $BA_DUA_CLI report -classes $BA_DUA_CLASSES_PATH -input $BA_DUA_SER -xml $BA_DUA_OUTXML
