fc69077ab45ad2fbf8d5df8e2570f3529d5286a9
StefanoBelli <stefano9913@gmail.com>
2024-08-19 18:35:23 +0200

    Disabling test

--- storm-client/test/jvm/org/apache/storm/windowing/WindowManagerGetEarliestEventTsTest.java
+++ storm-client/test/jvm/org/apache/storm/windowing/WindowManagerGetEarliestEventTsTest.java
@@ -1,6 +1,7 @@
 package org.apache.storm.windowing;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -10,6 +11,7 @@ import java.util.Arrays;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
+@Ignore
 @RunWith(Parameterized.class)
 public final class WindowManagerGetEarliestEventTsTest extends SutWindowManager {
     private final int startEvtNum;
